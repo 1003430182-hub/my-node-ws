7a993f864cbde78160bd5a9e29a723c1f2cf814c
qinhelili <35369353+qinhelili@users.noreply.github.com>
2025-12-11 23:07:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+
+> https://github.com/eooce/node-ws  
+> hug 分支
---
+++ worker.js
@@ -0,0 +1,19 @@
+export default {
+    async fetch(request, env) {
+        let url = new URL(request.url);
+        if (url.pathname.startsWith('/')) {
+            var arrStr = [
+                'change.your.domain', // 此处单引号里填写Hugging Face中容器提供的域名
+            ];
+            url.protocol = 'https:'
+            url.hostname = getRandomArray(arrStr)
+            let new_request = new Request(url, request);
+            return fetch(new_request);
+        }
+        return env.ASSETS.fetch(request);
+    },
+};
+function getRandomArray(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
